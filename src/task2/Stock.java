72b1c7f677061cf1cc730689b4369fca444313d4
Kirill <zalogi94@gmail.com>
2023-12-31 17:33:55 +0300

    Задача 2. Stock

---
+++ src/task2/Stock.java
@@ -0,0 +1,36 @@
+package task2;
+
+public class Stock {
+    private String symbol;
+    private String name;
+    private double previousClosingPrice;
+    private double currentPrice;
+
+    public Stock(String symbol, String name, double previousClosingPrice, double currentPrice) {
+        this.symbol = symbol;
+        this.name = name;
+        this.previousClosingPrice = previousClosingPrice;
+        this.currentPrice = currentPrice;
+    }
+
+    public double getChangePercent() {
+        double a = (currentPrice - previousClosingPrice)/previousClosingPrice;
+        return a;
+    }
+
+    @Override
+    public String toString() {
+        return "Stock{" +
+                "symbol='" + symbol + '\'' +
+                ", name='" + name + '\'' +
+                ", previousClosingPrice=" + previousClosingPrice + '\'' +
+                ", currentPrice=" + currentPrice + '\'' +
+                ", изменение= " + getChangePercent() +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Stock stock = new Stock("gpn", "gpromneft", 935.6, 847);
+        System.out.println(stock);
+    }
+}
