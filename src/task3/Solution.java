177e70becf8f8697a4e3fe775f0de0a451a3a3c2
Kirill-nk <kirill9414@yandex.ru>
2024-01-02 22:41:43 +0300

    JavaOOP task3

---
+++ src/task3/Solution.java
@@ -0,0 +1,13 @@
+package task3;
+import java.util.Date;
+
+public class Solution {
+    public static void main(String[] args) {
+        Date date = new Date();
+        date.setTime(10_000_000);
+        System.out.println(date);
+    }
+
+
+
+}
